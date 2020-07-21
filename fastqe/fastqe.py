5c850d708aaff5aecc22c0bf16f83c045abb1a48
cjwomack <39180413+cjwomack@users.noreply.github.com>
2020-07-22 01:37:28 +1000

    hopefully version will work the same

--- fastqe/fastqe.py
+++ fastqe/fastqe.py
@@ -588,7 +588,8 @@ def run_fastqe(fasta_files,minlen=0,scale=False,version=False,
                output=None,long=None,log=None,bin=False,fasta=False):
     options = Namespace(bin=bin, fasta=fasta, custom=custom, fasta_files=fasta_files,log=log, long=long, max=max, mean=mean, min=min, minlen=minlen, noemoji=noemoji, output=output, scale=scale, version=True)
     if options.version:
-        pass
+        print(PROGRAM_NAME + PROGRAM_VERSION)
+        exit()
     process_files(options)
         
 def main():
